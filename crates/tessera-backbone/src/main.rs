b4681a6bb8ef6185b7ff9bd36f549cfc61863648
boris-cremit <boris@cremit.io>
2024-10-04 15:25:42 +0900

    input commandline arguments

--- crates/tessera-backbone/src/main.rs
+++ crates/tessera-backbone/src/main.rs
@@ -1,11 +1,24 @@
-use tracing::info;
+use std::path::{Path, PathBuf};
+
+use clap::{Arg, Parser};
+use tracing::{debug, info};
 
 use crate::logger::LoggerConfig;
 
 mod logger;
 
+#[derive(Parser, Debug)]
+#[command(version, about)]
+struct Args {
+    /// Sets a custom config file
+    #[arg(short, long, value_name = "FILE")]
+    pub config: Option<PathBuf>,
+}
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    let args = Args::parse();
+
     logger::init_logger(LoggerConfig::default());
     info!("Hello, world!");
     Ok(())
