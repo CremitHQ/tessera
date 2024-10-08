ce63affc8b016889a88f470d2741ed1206f8cc38
boris-cremit <boris@cremit.io>
2024-10-08 21:41:46 +0900

    remove trailing slash of workspace api

--- crates/tessera-backbone/src/server/mod.rs
+++ crates/tessera-backbone/src/server/mod.rs
@@ -5,6 +5,7 @@ use tracing::debug;
 
 use crate::{application::Application, config::ApplicationConfig};
 
+mod response;
 mod router;
 
 pub(super) struct ServerConfig {
@@ -19,7 +20,7 @@ impl From<&ApplicationConfig> for ServerConfig {
 
 pub(super) async fn run(application: Application, config: ServerConfig) -> anyhow::Result<()> {
     let application = Arc::new(application);
-    let app = Router::new().nest("/workspaces/", router::workspace::router(application.clone()));
+    let app = Router::new().nest("/workspaces", router::workspace::router(application.clone()));
     let listener = tokio::net::TcpListener::bind(("0.0.0.0", config.port)).await?;
     debug!("starting backbone server on {}", config.port);
     axum::serve(listener, app).await?;
