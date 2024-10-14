b39f794a4da81f5a40dc2225100623cb71a5b9f4
john-cremit <john@cremit.io>
2024-10-14 22:20:56 +0900

    fix: update dependencies and enhance Shield trait

--- crates/tessera-storage/src/shield/mod.rs
+++ crates/tessera-storage/src/shield/mod.rs
@@ -1,11 +1,14 @@
+use zeroize::ZeroizeOnDrop;
+
 pub mod aes;
 
 #[trait_variant::make(Shield: Send)]
 pub trait LocalShield {
     type ShieldError: std::error::Error;
     type Key: ToOwned + ?Sized;
+    type ZeroizingKey: ZeroizeOnDrop;
     async fn initialize(&self, master_key: &Self::Key) -> Result<(), Self::ShieldError>;
     async fn armor(&self) -> Result<(), Self::ShieldError>;
     async fn disarm(&self, master_key: &Self::Key) -> Result<(), Self::ShieldError>;
-    async fn generate_key(&self) -> Result<<Self::Key as ToOwned>::Owned, Self::ShieldError>;
+    async fn generate_key(&self) -> Result<Self::ZeroizingKey, Self::ShieldError>;
 }
