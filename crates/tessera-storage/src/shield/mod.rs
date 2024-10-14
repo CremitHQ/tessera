24bb50314854834fdf489d0303448452eda87bfc
john-cremit <john@cremit.io>
2024-10-14 20:31:10 +0900

    chore: rename and revamp key type for Shield trait methods

--- crates/tessera-storage/src/shield/mod.rs
+++ crates/tessera-storage/src/shield/mod.rs
@@ -3,10 +3,9 @@ pub mod aes;
 #[trait_variant::make(Shield: Send)]
 pub trait LocalShield {
     type ShieldError: std::error::Error;
-    type Key: ?Sized;
-    type Value: ToOwned + ?Sized;
-    async fn initialize(&self, key: &Self::Value) -> Result<(), Self::ShieldError>;
+    type Key: ToOwned + ?Sized;
+    async fn initialize(&self, master_key: &Self::Key) -> Result<(), Self::ShieldError>;
     async fn armor(&self) -> Result<(), Self::ShieldError>;
-    async fn disarm(&self, key: &Self::Value) -> Result<(), Self::ShieldError>;
-    async fn generate_key(&self) -> Result<<Self::Value as ToOwned>::Owned, Self::ShieldError>;
+    async fn disarm(&self, master_key: &Self::Key) -> Result<(), Self::ShieldError>;
+    async fn generate_key(&self) -> Result<<Self::Key as ToOwned>::Owned, Self::ShieldError>;
 }
