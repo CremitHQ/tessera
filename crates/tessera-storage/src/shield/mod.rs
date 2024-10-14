6cb8dc1efc759a34b57839466f884dc3d189da8c
john-cremit <john@cremit.io>
2024-10-14 16:48:46 +0900

    refactor: change key type in Shield trait methods

--- crates/tessera-storage/src/shield/mod.rs
+++ crates/tessera-storage/src/shield/mod.rs
@@ -5,8 +5,8 @@ pub trait LocalShield {
     type ShieldError: std::error::Error;
     type Key: ?Sized;
     type Value: ToOwned + ?Sized;
-    async fn initialize(&self, key: &Self::Key) -> Result<(), Self::ShieldError>;
+    async fn initialize(&self, key: &Self::Value) -> Result<(), Self::ShieldError>;
     async fn armor(&self) -> Result<(), Self::ShieldError>;
-    async fn disarm(&self, key: &Self::Key) -> Result<(), Self::ShieldError>;
+    async fn disarm(&self, key: &Self::Value) -> Result<(), Self::ShieldError>;
     async fn generate_key(&self) -> Result<<Self::Value as ToOwned>::Owned, Self::ShieldError>;
 }
