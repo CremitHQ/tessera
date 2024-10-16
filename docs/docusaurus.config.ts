39616ab1ee212b9f1eb4cc3f3d0888f082816006
rene-cremit <161474120+rene-cremit@users.noreply.github.com>
2024-10-16 23:18:20 +0900

    docs: Update social card image (#31)

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -49,8 +49,7 @@ const config: Config = {
   ],
 
   themeConfig: {
-    // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: 'img/social-card.jpg',
     navbar: {
       title: 'Tessera',
       logo: {
