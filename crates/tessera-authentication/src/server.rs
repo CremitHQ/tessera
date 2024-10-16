1c28c1cc1b7ecae56ab17b7b874f222d39fcaa50
john-cremit <john@cremit.io>
2024-10-16 16:52:35 +0900

    feat(auth): initialize authentication server

---
+++ crates/tessera-authentication/src/server.rs
@@ -0,0 +1,22 @@
+use axum::Router;
+use tracing::debug;
+
+use crate::config::ApplicationConfig;
+
+pub(super) struct ServerConfig {
+    pub port: u16,
+}
+
+impl From<&ApplicationConfig> for ServerConfig {
+    fn from(value: &ApplicationConfig) -> Self {
+        Self { port: value.port }
+    }
+}
+
+pub(super) async fn run(config: ServerConfig) -> anyhow::Result<()> {
+    let app = Router::new();
+    let listener = tokio::net::TcpListener::bind(("0.0.0.0", config.port)).await?;
+    debug!("starting authentication server on {}", config.port);
+    axum::serve(listener, app).await?;
+    Ok(())
+}
