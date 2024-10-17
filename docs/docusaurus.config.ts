ff276afc243b16850b2c9a2c683318a416f9892e
Rene Bak <rene@cremit.io>
2024-10-17 15:46:09 +0900

    build: Update production URL in docusaurus config to docs.cremit.io

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -8,16 +8,16 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://cremithq.github.io',
+  url: 'https://docs.cremit.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/tessera',
+  trailingSlash: false,
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'CremitHQ', // Usually your GitHub org/user name.
   projectName: 'tessera', // Usually your repo name.
-  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
