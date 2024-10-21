5815c5b487dcbfdcbfaee59f03f058f168586f80
john-cremit <john@cremit.io>
2024-10-21 21:55:08 +0900

    feat(backbone): implement parameter creation endpoint and response handling

---
+++ crates/tessera-backbone/src/server/router/parameter/mod.rs
@@ -0,0 +1,40 @@
+use std::sync::Arc;
+
+use axum::{
+    debug_handler,
+    extract::{Path, State},
+    response::IntoResponse,
+    routing::post,
+    Json, Router,
+};
+
+use crate::application::{
+    self,
+    parameter::{ParameterData, ParameterUseCase},
+    Application,
+};
+
+use self::response::ParameterResponse;
+
+mod response;
+
+pub(crate) fn router(application: Arc<Application>) -> axum::Router {
+    Router::new().route("/workspaces/:workspace_name/parameter", post(handle_create_parameter)).with_state(application)
+}
+
+#[debug_handler]
+async fn handle_create_parameter(
+    Path(workspace_name): Path<String>,
+    State(application): State<Arc<Application>>,
+) -> Result<impl IntoResponse, application::parameter::Error> {
+    let parameter = application.with_workspace(&workspace_name).parameter().create().await?;
+    let response: ParameterResponse = parameter.into();
+
+    Ok(Json(response))
+}
+
+impl From<ParameterData> for ParameterResponse {
+    fn from(value: ParameterData) -> Self {
+        Self { version: value.version, parameter: value.value }
+    }
+}
