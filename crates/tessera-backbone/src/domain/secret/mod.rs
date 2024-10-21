49e559afa964400e238b6ac543ce5ea73a5e3147
boris-cremit <boris@cremit.io>
2024-10-21 21:30:38 +0900

    test secret service

--- crates/tessera-backbone/src/domain/secret/mod.rs
+++ crates/tessera-backbone/src/domain/secret/mod.rs
@@ -48,6 +48,8 @@ fn parse_identifier(full_path: &str) -> Option<(String, String)> {
     let mut capture = IDENTIFIER_PATTERN.captures_iter(full_path);
     let (_, [path, key]) = capture.next().map(|c| c.extract())?;
 
+    let path = if path == "" { "/".to_owned() } else { path.to_owned() };
+
     Some((path.to_owned(), key.to_owned()))
 }
 
@@ -168,7 +170,7 @@ mod test {
     }
 
     #[tokio::test]
-    async fn when_getting_secret_is_failed_then_secret_service_returns_anyhow_err() {
+    async fn when_getting_secrets_is_failed_then_secret_service_returns_anyhow_err() {
         let mock_database = MockDatabase::new(DatabaseBackend::Postgres)
             .append_query_errors(vec![DbErr::Custom("some error".to_owned())]);
         let mock_connection = Arc::new(mock_database.into_connection());
@@ -182,5 +184,113 @@ mod test {
 
         assert!(matches!(result, Err(Error::Anyhow(_))));
         assert_eq!(result.err().unwrap().to_string(), "Custom Error: some error");
+    }
+
+    #[tokio::test]
+    async fn when_getting_secret_data_is_successful_then_secret_service_returns_secret_ok() {
+        let now = Utc::now();
+        let identifier = "/test/path/TEST_KEY";
+        let metadata_id = UlidId::new(Ulid::from_str("01JACYVTYB4F2PEBFRG1BB7BKP").unwrap());
+        let key = "TEST_KEY";
+        let path = "/test/path";
+        let applied_policy_ids = [
+            UlidId::new(Ulid::from_str("01JACZ1B5W5Z3D9R1CVYB7JJ8S").unwrap()),
+            UlidId::new(Ulid::from_str("01JACZ1FG1RYABQW2KB6YSEZ84").unwrap()),
+        ];
+        let policy_id = UlidId::new(Ulid::from_str("01JACZ44MJDY5GD21X2W910CFV").unwrap());
+
+        let mock_database = MockDatabase::new(DatabaseBackend::Postgres)
+            .append_query_results([vec![secret_metadata::Model {
+                id: metadata_id.to_owned(),
+                key: key.to_owned(),
+                path: path.to_owned(),
+                created_at: now,
+                updated_at: now,
+            }]])
+            .append_query_results([vec![
+                applied_policy::Model {
+                    id: applied_policy_ids[0].to_owned(),
+                    secret_metadata_id: metadata_id.to_owned(),
+                    r#type: applied_policy::PolicyApplicationType::Read,
+                    policy_id: policy_id.to_owned(),
+                    created_at: now,
+                    updated_at: now,
+                },
+                applied_policy::Model {
+                    id: applied_policy_ids[1].to_owned(),
+                    secret_metadata_id: metadata_id.to_owned(),
+                    r#type: applied_policy::PolicyApplicationType::Write,
+                    policy_id: policy_id.to_owned(),
+                    created_at: now,
+                    updated_at: now,
+                },
+            ]]);
+
+        let mock_connection = Arc::new(mock_database.into_connection());
+
+        let secret_service = PostgresSecretService {};
+
+        let transaction = mock_connection.begin().await.expect("begining transaction should be successful");
+
+        let result =
+            secret_service.get(&transaction, identifier).await.expect("creating workspace should be successful");
+        transaction.commit().await.expect("commiting transaction should be successful");
+
+        assert_eq!(result.key, key);
+        assert_eq!(result.path, path);
+        assert_eq!(result.reader_policy_ids[0], Ulid::from_str("01JACZ1B5W5Z3D9R1CVYB7JJ8S").unwrap());
+        assert_eq!(result.writer_policy_ids[0], Ulid::from_str("01JACZ1FG1RYABQW2KB6YSEZ84").unwrap());
+    }
+
+    #[tokio::test]
+    async fn when_getting_secret_is_failed_then_secret_service_returns_anyhow_err() {
+        let identifier = "/some/secret";
+        let mock_database = MockDatabase::new(DatabaseBackend::Postgres)
+            .append_query_errors(vec![DbErr::Custom("some error".to_owned())]);
+        let mock_connection = Arc::new(mock_database.into_connection());
+
+        let secret_service = PostgresSecretService {};
+
+        let transaction = mock_connection.begin().await.expect("begining transaction should be successful");
+
+        let result = secret_service.get(&transaction, identifier).await;
+        transaction.commit().await.expect("commiting transaction should be successful");
+
+        assert!(matches!(result, Err(Error::Anyhow(_))));
+        assert_eq!(result.err().unwrap().to_string(), "Custom Error: some error");
+    }
+
+    #[tokio::test]
+    async fn when_getting_secret_path_without_slash_then_secret_service_returns_invalid_secret_identifier_error() {
+        let identifier = "just_key";
+        let mock_database = MockDatabase::new(DatabaseBackend::Postgres)
+            .append_query_errors(vec![DbErr::Custom("some error".to_owned())]);
+        let mock_connection = Arc::new(mock_database.into_connection());
+
+        let secret_service = PostgresSecretService {};
+
+        let transaction = mock_connection.begin().await.expect("begining transaction should be successful");
+
+        let result = secret_service.get(&transaction, identifier).await;
+        transaction.commit().await.expect("commiting transaction should be successful");
+
+        assert!(matches!(result, Err(Error::InvalidSecretIdentifier { .. })));
+    }
+
+    #[tokio::test]
+    async fn when_getting_not_existing_secret_then_secret_service_returns_secret_not_exists_error() {
+        let identifier = "/some/secret";
+        let mock_database =
+            MockDatabase::new(DatabaseBackend::Postgres).append_query_results([Vec::<secret_metadata::Model>::new()]);
+        let mock_connection = Arc::new(mock_database.into_connection());
+
+        let secret_service = PostgresSecretService {};
+
+        let transaction = mock_connection.begin().await.expect("begining transaction should be successful");
+
+        let result = secret_service.get(&transaction, identifier).await;
+        transaction.commit().await.expect("commiting transaction should be successful");
+
+        assert!(matches!(result, Err(Error::SecretNotExists { .. })));
     }
 }
