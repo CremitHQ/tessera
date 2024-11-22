eecea4a5caaa5e1cd967a5038f95baee8c38beff
john-cremit <john@cremit.io>
2024-11-22 19:59:45 +0900

    refactor(storage): update keys for conditional zeroize support

--- crates/nebula-storage/src/shield/mod.rs
+++ crates/nebula-storage/src/shield/mod.rs
@@ -1,15 +1,30 @@
+#[cfg(feature = "zeroize")]
 use zeroize::ZeroizeOnDrop;
 
 pub mod aes;
 
+#[cfg(feature = "zeroize")]
 #[trait_variant::make(Shield: Send)]
 pub trait LocalShield {
     type ShieldError: std::error::Error;
     type Key: ToOwned + ?Sized;
-    type ZeroizingKey: ZeroizeOnDrop;
+    type ShieldKey: ZeroizeOnDrop;
     async fn is_initialized(&self) -> Result<bool, Self::ShieldError>;
     async fn initialize(&self, master_key: &Self::Key) -> Result<(), Self::ShieldError>;
     async fn armor(&self) -> Result<(), Self::ShieldError>;
     async fn disarm(&self, master_key: &Self::Key) -> Result<(), Self::ShieldError>;
-    async fn generate_key(&self) -> Result<Self::ZeroizingKey, Self::ShieldError>;
+    async fn generate_key(&self) -> Result<Self::ShieldKey, Self::ShieldError>;
+}
+
+#[cfg(not(feature = "zeroize"))]
+#[trait_variant::make(Shield: Send)]
+pub trait LocalShield {
+    type ShieldError: std::error::Error;
+    type Key: ToOwned + ?Sized;
+    type ShieldKey;
+    async fn is_initialized(&self) -> Result<bool, Self::ShieldError>;
+    async fn initialize(&self, master_key: &Self::Key) -> Result<(), Self::ShieldError>;
+    async fn armor(&self) -> Result<(), Self::ShieldError>;
+    async fn disarm(&self, master_key: &Self::Key) -> Result<(), Self::ShieldError>;
+    async fn generate_key(&self) -> Result<Self::ShieldKey, Self::ShieldError>;
 }
