1a20879540ba2364570eb477fb98ea27bf7828bd
john-cremit <john@cremit.io>
2024-11-28 00:03:16 +0900

    refactor(router): simplify path structure & require entity_id

--- crates/nebula-authorization/src/application.rs
+++ crates/nebula-authorization/src/application.rs
@@ -29,7 +29,7 @@ impl Application {
                 .redirect_uri(config.base_url.join("/callback/saml")?)
                 .maybe_sso_url(saml.sso_url.as_ref())
                 .maybe_idp_issuer(saml.idp_issuer.as_ref())
-                .maybe_entity_id(saml.entity_id.as_ref())
+                .entity_id(&saml.entity_id)
                 .ca(openssl::x509::X509::from_pem(saml.ca.as_bytes())?)
                 .attributes_config(saml.attributes.clone())
                 .workspace_config(config.workspace.clone())
--- crates/nebula-authorization/src/server/mod.rs
+++ crates/nebula-authorization/src/server/mod.rs
@@ -9,17 +9,22 @@ mod router;
 
 pub(super) struct ServerConfig {
     pub port: u16,
+    pub path_prefix: Option<String>,
 }
 
 impl From<ApplicationConfig> for ServerConfig {
     fn from(value: ApplicationConfig) -> Self {
-        Self { port: value.port }
+        Self { port: value.port, path_prefix: value.path_prefix }
     }
 }
 
 pub(super) async fn run(application: Application, config: ServerConfig) -> anyhow::Result<()> {
     let application = Arc::new(application);
-    let app = Router::new().route("/health", get(|| async { "" })).nest("/", router::router(application.clone()));
+    let app = Router::new().route("/health", get(|| async { "" }));
+    let app = app.nest(
+        if let Some(ref path_prefix) = config.path_prefix { path_prefix } else { "/" },
+        router::router(application.clone()),
+    );
 
     let listener = tokio::net::TcpListener::bind(("0.0.0.0", config.port)).await?;
     debug!("starting authz server on {}", config.port);
