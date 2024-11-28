a5483a2938df14c18345461f4a13aa8ce993a31f
john-cremit <146056759+john-cremit@users.noreply.github.com>
2024-11-28 20:30:10 +0900

    docs: fix link handling (#125)

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -3,81 +3,81 @@ import type { Config } from "@docusaurus/types";
 import type * as Preset from "@docusaurus/preset-classic";
 
 const config: Config = {
-	title: "Nebula",
-	tagline:
-		"A secret manager offering robust Attribute-Based Encryption (ABE) for secure data protection.",
-	favicon: "img/favicon.ico",
+  title: "Nebula",
+  tagline:
+    "A secret manager offering robust Attribute-Based Encryption (ABE) for secure data protection.",
+  favicon: "img/favicon.ico",
 
-	// Set the production url of your site here
-	url: "https://docs.cremit.io",
-	// Set the /<baseUrl>/ pathname under which your site is served
-	// For GitHub pages deployment, it is often '/<projectName>/'
-	baseUrl: "/nebula",
-	trailingSlash: false,
+  // Set the production url of your site here
+  url: "https://docs.cremit.io",
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: "/nebula",
+  trailingSlash: false,
 
-	// GitHub pages deployment config.
-	// If you aren't using GitHub pages, you don't need these.
-	organizationName: "CremitHQ", // Usually your GitHub org/user name.
-	projectName: "nebula", // Usually your repo name.
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: "CremitHQ", // Usually your GitHub org/user name.
+  projectName: "nebula", // Usually your repo name.
 
-	onBrokenLinks: "throw",
-	onBrokenMarkdownLinks: "warn",
+  onBrokenLinks: "warn",
+  onBrokenMarkdownLinks: "warn",
 
-	// Even if you don't use internationalization, you can use this field to set
-	// useful metadata like html lang. For example, if your site is Chinese, you
-	// may want to replace "en" with "zh-Hans".
-	i18n: {
-		defaultLocale: "en",
-		locales: ["en"],
-	},
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en"],
+  },
 
-	presets: [
-		[
-			"classic",
-			{
-				docs: {
-					sidebarPath: "./sidebars.ts",
-					// Please change this to your repo.
-					// Remove this to remove the "edit this page" links.
-					editUrl: "https://github.com/CremitHQ/nebula/tree/main/docs/docs/",
-				},
-				theme: {
-					customCss: "./src/css/custom.css",
-				},
-			} satisfies Preset.Options,
-		],
-	],
+  presets: [
+    [
+      "classic",
+      {
+        docs: {
+          sidebarPath: "./sidebars.ts",
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl: "https://github.com/CremitHQ/nebula/tree/main/docs/docs/",
+        },
+        theme: {
+          customCss: "./src/css/custom.css",
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
 
-	themeConfig: {
-		image: "img/social-card.jpg",
-		navbar: {
-			title: "Nebula",
-			logo: {
-				alt: "Nebula Logo",
-				src: "img/logo.webp",
-			},
-			items: [
-				{
-					type: "docSidebar",
-					sidebarId: "docsSidebar",
-					position: "left",
-					label: "Docs",
-				},
-				{
-					href: "https://github.com/CremitHQ/nebula",
-					label: "GitHub",
-					position: "right",
-				},
-			],
-		},
-		footer: {
-			copyright: `© Cremit, Inc.`,
-		},
-		prism: {
-			theme: prismThemes.github,
-			darkTheme: prismThemes.dracula,
-		},
-	} satisfies Preset.ThemeConfig,
+  themeConfig: {
+    image: "img/social-card.jpg",
+    navbar: {
+      title: "Nebula",
+      logo: {
+        alt: "Nebula Logo",
+        src: "img/logo.webp",
+      },
+      items: [
+        {
+          type: "docSidebar",
+          sidebarId: "docsSidebar",
+          position: "left",
+          label: "Docs",
+        },
+        {
+          href: "https://github.com/CremitHQ/nebula",
+          label: "GitHub",
+          position: "right",
+        },
+      ],
+    },
+    footer: {
+      copyright: `© Cremit, Inc.`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
 };
 
 export default config;
