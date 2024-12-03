8af9bbf13f2d197c1f6a63b5083a68b5f6cb187b
john-cremit <john@cremit.io>
2024-12-03 16:54:03 +0900

    feat(cli): add success message on login completion

--- crates/nebula-cli/src/command/login.rs
+++ crates/nebula-cli/src/command/login.rs
@@ -1,7 +1,11 @@
+use std::io::stdout;
+
 use async_trait::async_trait;
 use clap::Args;
+use crossterm::execute;
+use crossterm::style::{Color, Print, ResetColor, SetForegroundColor};
 
-use crate::api::get_token_from_machine_identity_token;
+use crate::api::authorization::get_token_from_machine_identity_token;
 use crate::config::{save_token, AuthorizationMethod, NebulaConfig};
 
 use super::{GlobalArgs, RunCommand};
@@ -27,6 +31,7 @@ impl RunCommand for LoginCommand {
             }
         };
 
+        execute!(stdout(), SetForegroundColor(Color::Green), Print("✅ Successfully logged in!\n"), ResetColor)?;
         Ok(())
     }
 }
