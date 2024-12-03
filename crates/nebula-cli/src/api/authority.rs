e9a71f8de66d3c3c1fb65f140b469aa32538ee33
john-cremit <john@cremit.io>
2024-12-03 16:53:16 +0900

    feat: add API functions for each components(backbone,authority,authz)

---
+++ crates/nebula-cli/src/api/authority.rs
@@ -0,0 +1,46 @@
+use cached::proc_macro::io_cached;
+use reqwest::IntoUrl;
+use serde::{Deserialize, Serialize};
+
+#[derive(Clone, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct GetPublicKeyResponse {
+    pub public_key: String,
+    pub version: u64,
+}
+
+#[io_cached(
+    map_error = "|e| anyhow::anyhow!(e)",
+    disk = true,
+    time = 60,
+    key = "String",
+    convert = r#"{ format!("pk:{}/{}", authority_url.as_str(), workspace_name) }"#
+)]
+pub async fn get_public_key(authority_url: impl IntoUrl, workspace_name: &str) -> anyhow::Result<GetPublicKeyResponse> {
+    let client = reqwest::Client::new();
+
+    let url = authority_url.into_url()?.join(&format!("workspaces/{workspace_name}/public-key"))?;
+    let response = client.get(url).send().await?.json::<GetPublicKeyResponse>().await?;
+
+    Ok(response)
+}
+
+#[derive(Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct GetUserKeyResponse {
+    pub user_key: String,
+    pub version: u64,
+}
+
+pub async fn get_user_key(
+    authority_url: impl IntoUrl,
+    workspace_name: &str,
+    token: &str,
+) -> anyhow::Result<GetUserKeyResponse> {
+    let client = reqwest::Client::new();
+
+    let url = authority_url.into_url()?.join(&format!("workspaces/{workspace_name}/user-key"))?;
+    let response = client.get(url).bearer_auth(token).send().await?.json::<GetUserKeyResponse>().await?;
+
+    Ok(response)
+}
