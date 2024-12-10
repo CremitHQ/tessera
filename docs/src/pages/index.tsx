110fa08043b8378dfc0d829ac9ac346c55f5d934
john-cremit <john@cremit.io>
2024-12-10 13:35:02 +0900

    docs: add Nebula CLI usage

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -1,16 +1,16 @@
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
-import Heading from '@theme/Heading';
-import Layout from '@theme/Layout';
-import clsx from 'clsx';
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import HomepageFeatures from "@site/src/components/HomepageFeatures";
+import Heading from "@theme/Heading";
+import Layout from "@theme/Layout";
+import clsx from "clsx";
 
-import styles from './index.module.css';
+import styles from "./index.module.css";
 
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
   return (
-    <header className={clsx('main__header', styles.mainHeader)}>
+    <header className={clsx("main__header", styles.mainHeader)}>
       <Heading as="h1" className={styles.title}>
         {siteConfig.title}
       </Heading>
@@ -18,7 +18,7 @@ function HomepageHeader() {
       <div className={styles.buttons}>
         <Link
           className="button button--secondary button--lg"
-          to="/docs/overview"
+          to="/docs/getting-started/introduction"
         >
           Read Docs
         </Link>
